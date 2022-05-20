d8a08c0163e4f8763da6f70fef6e467cd38eb7a9
JohnRay1997 <johnkomarnickicontact@gmail.com>
2022-05-19 21:54:29 -0400

    base input component

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <BaseInput v-model="firstName" type="text" class="base-input" />
+
+    <p>{{ firstName }}</p>
+  </div>
+</template>
+
+<script setup>
+import BaseInput from "./components/BaseInput.vue";
+import { ref } from "vue";
+
+const firstName = ref("");
+</script>
+
+<style>
+#app {
+  max-width: 450px;
+  margin: 60px auto;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="input-wrap">
+    <label v-if="label">{{ label }}</label>
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: [String, Boolean],
+    default: false,
+  },
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  flex-direction: column;
+
+  input {
+    padding: 8px 12px;
+    font-size: 16px;
+  }
+}
+</style>
